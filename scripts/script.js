8435c8aa9854094e980a7f17fc4eda29c6859bf2
Balybin Stanislav <balybins@mail.ru>
2020-06-05 17:10:29 +0600

    Вывел в консоль длину строки

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let money = 70000;
 let income = 'Разведение кроликов';
-let addExpenses = 'Казино, блекджек и кролики!';
+let addExpenses = 'Казино, блекджек, кролики';
 let deposit = true;
 let mission = 10000000;
 let period = 6;
@@ -8,3 +8,5 @@ let period = 6;
 console.log('money: ', typeof money);
 console.log('income: ', typeof income);
 console.log('deposit: ', typeof deposit);
+
+console.log('Length addExpenses: ', addExpenses.length);
