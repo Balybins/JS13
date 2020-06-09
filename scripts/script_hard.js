5a4acc44754120af090cc6782e841eae672e6ab2
Stanislav Balybin <balybins@mail.ru>
2020-06-10 04:05:47 +0600

    Добавил в усложненное задание решение с помощью callbeck функции

--- scripts/script_hard.js
+++ scripts/script_hard.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let data = ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga blanditiis eius earum autem similique modi, totam magni ab ipsum nisi. Quam voluptatum magnam explicabo delectus facilis laborum voluptate dolor culpa? ';
+// let data = 12345;
+
+// Обычная функция
 let someFunction = function (data){
     if (typeof data !== 'string'){
         return 'Это не строка! Введите корректные данные!';
@@ -11,7 +15,26 @@ let someFunction = function (data){
         } else {
             return changeData;
         }
-        
     }
 }
-console.log(someFunction(' Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga blanditiis eius earum autem similique modi, totam magni ab ipsum nisi. Quam voluptatum magnam explicabo delectus facilis laborum voluptate dolor culpa? '));
+console.log(someFunction(data));
+
+// Callbeck функция
+let someFunction2 = function (data, callbeck){
+    if (typeof data !== 'string'){
+        console.log('Это не строка! Введите корректные данные!');
+    } else {
+        callbeck(data);
+    }
+}
+
+function stringData(data){
+    let changeData = data.trim();
+        if (changeData.length > 30){
+            let resultData = changeData.slice(0, 30) + '...';
+            console.log(resultData);
+        } else {
+            console.log(changeData);
+        }
+}
+someFunction2(data, stringData);
