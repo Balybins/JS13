c0721ede53020aeb7727ff858b7cdef5d7d4f2db
Stanislav Balybin <balybins@mail.ru>
2020-06-11 06:35:42 +0600

    Исправил 1й пункт, выполнил 2й пункт усложненного задания

--- scripts/lesson_hard.js
+++ scripts/lesson_hard.js
@@ -3,7 +3,7 @@
 let arr = [],
     haveNumber = null;
 
-for (let i = 0; i < 2; i++){
+for (let i = 0; i < 8; i++){
     let number = prompt('Введите многозначное число');
     while (number.length < 2) {
         number = prompt('Введите многозначное число');
@@ -20,4 +20,19 @@ for (let numbers of arr){
 }
 if (haveNumber !== 1) {
     console.log('Чисел начинающихся на 2 или 4 - нет в массиве');
+}
+
+console.warn('Второе задание:');
+nextStep:
+for (let i = 2; i <= 100; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0){
+        continue nextStep;
+    } 
+  }
+  if (i === 73){
+    console.log('Число:', i + '. Делители этого числа - 1 и ' + i + ' и это любимое число Шелдона Купера!');
+  } else {
+    console.log('Число:', i + '. Делители этого числа - 1 и ' + i);
+  }
 }
