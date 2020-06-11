7ce0c69858edb01f679bd970028c83d5c89f1fa4
Balybin Stanislav <balybins@mail.ru>
2020-06-11 09:58:53 +0600

    Добавил ещё одно решение 2го пункта усложненного задания

--- scripts/lesson_hard.js
+++ scripts/lesson_hard.js
@@ -1,38 +1,55 @@
 'use strict';
 
 let arr = [],
-    haveNumber = null;
+  haveNumber = null;
 
-for (let i = 0; i < 8; i++){
-    let number = prompt('Введите многозначное число');
-    while (number.length < 2) {
-        number = prompt('Введите многозначное число');
-    }
-    arr[i] = number;
+for (let i = 0; i < 8; i++) {
+  let number = prompt('Введите многозначное число');
+  while (number.length < 2) {
+    number = prompt('Введите многозначное число');
+  }
+  arr[i] = number;
 }
 
 console.warn('Первое задание:');
-for (let numbers of arr){
-    if (numbers.slice(0, 1) === '2' || numbers.slice(0, 1) === '4'){
-        console.log(numbers);
-        haveNumber = 1;
-    }
+for (let numbers of arr) {
+  if (numbers.slice(0, 1) === '2' || numbers.slice(0, 1) === '4') {
+    console.log(numbers);
+    haveNumber = 1;
+  }
 }
 if (haveNumber !== 1) {
-    console.log('Чисел начинающихся на 2 или 4 - нет в массиве');
+  console.log('Чисел начинающихся на 2 или 4 - нет в массиве');
 }
 
-console.warn('Второе задание:');
+console.warn('Второе задание (подсмотрел на learn.javascript.ru):');
 nextStep:
-for (let i = 2; i <= 100; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j === 0){
+  for (let i = 2; i <= 100; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
         continue nextStep;
-    } 
+      }
+    }
+    if (i === 73) {
+      console.log('Число:', i + '. Делители этого числа - 1 и ' + i + ' и это любимое число Шелдона Купера!');
+    } else {
+      console.log('Число:', i + '. Делители этого числа - 1 и ' + i);
+    }
   }
-  if (i === 73){
-    console.log('Число:', i + '. Делители этого числа - 1 и ' + i + ' и это любимое число Шелдона Купера!');
-  } else {
-    console.log('Число:', i + '. Делители этого числа - 1 и ' + i);
+
+console.warn('Второе задание (самостоятельное исполнение):');
+for (let i = 2; i <= 100; i++) {
+  let count = 0;
+  for (let j = 2; j <= i; j++) {
+    if (i % j === 0) {
+      count++;
+    }
+  }
+  if (count < 2) {
+    if (i === 73) {
+      console.log('Число:', i + '. Делители этого числа - 1 и ' + i + ' и это любимое число Шелдона Купера!');
+    } else {
+      console.log('Число:', i + '. Делители этого числа - 1 и ' + i);
+    }
   }
 }
