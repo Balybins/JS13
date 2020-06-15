3e88a10220d0b58e4e8e658d6bd2eaa50d2bfe4e
Balybin Stanislav <balybins@mail.ru>
2020-06-15 10:08:25 +0600

    Изменил спопсоб вывода дней недели (вместо консоли на страницу)

--- scripts/script_hard.js
+++ scripts/script_hard.js
@@ -1,28 +1,32 @@
 'use strict';
-
 let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
     today,
     date = new Date();
 
-    if (date.getDay() === 0) {
-        today = 6;
-    } else {
-        today = date.getDay() - 1;
-    }
+// Адаптация под мой массив с днями недели
+if (date.getDay() === 0) {
+    today = 6;
+} else {
+    today = date.getDay() - 1;
+}
 
-let list = function() {
-    for (let day of week){
+// Функция вывода списка дней недели
+let list = function () {
+    const ul = document.createElement('ul');
+    for (let day of week) {
+        const li = document.createElement('li');
+        li.textContent = day;
         if (day === 'суббота' || day === 'воскресенье') {
-            if (week[today] === 'суббота' || week[today] === 'воскресенье'){
-                console.log('%c' + day, 'font-style: italic; font-weight: bold');
+            if (week[today] === 'суббота' || week[today] === 'воскресенье') {
+                li.classList.add('weekend', 'today');
             } else {
-                console.log('%c' + day, 'font-style: italic');
+                li.classList.add('weekend');
             }
-        } else if (day === week[today]){
-            console.log('%c' + day, 'font-weight: bold');
-        } else {
-            console.log(day);
+        } else if (day === week[today]) {
+            li.classList.add('today');
         }
+        ul.appendChild(li);
     }
+    return ul;
 };
-list();
+document.querySelector('#daysOfWeek').appendChild(list());
