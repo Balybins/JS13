619a9038a69548fd587952e8bbca5f629597757a
Balybin Stanislav <balybins@mail.ru>
2020-06-30 18:10:28 +0600

    Запустл муху

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 'use strict';
-console.log(123);
+
+let fly = document.getElementById('fly'),
+    count = 0,
+    flyInterval;
+
+let flyAnimate = () => {
+    flyInterval = requestAnimationFrame(flyAnimate);
+    count++;
+    if (count < 350) {
+        fly.style.left = count * 2 + 'px';
+        fly.style.top = count + 'px';
+    } else if (count < 500) {
+        fly.style.left = count * 2 + 'px';
+    } else {
+        cancelAnimationFrame(flyInterval);
+    }
+    console.log(count);
+};
+flyInterval = requestAnimationFrame(flyAnimate);
