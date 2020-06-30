2408304eae07d075201708a94811eb8cca039026
Stanislav Balybin <balybins@mail.ru>
2020-07-01 01:00:44 +0600

    Выполнил 2е дополнительное задание 18го урока

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,42 @@
 'use strict';
 
 let fly = document.getElementById('fly'),
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    reset = document.getElementById('reset'),
     count = 0,
     flyInterval;
 
 let flyAnimate = () => {
     flyInterval = requestAnimationFrame(flyAnimate);
     count++;
-    if (count < 350) {
+    if (count < 320) {
         fly.style.left = count * 2 + 'px';
         fly.style.top = count + 'px';
-    } else if (count < 500) {
+    } else if (count < 590) {
         fly.style.left = count * 2 + 'px';
     } else {
         cancelAnimationFrame(flyInterval);
     }
-    console.log(count);
 };
-flyInterval = requestAnimationFrame(flyAnimate);
+
+start.addEventListener('click', () => {
+    flyInterval = requestAnimationFrame(flyAnimate);
+    stop.style.display = 'block';
+    start.style.display = 'none';
+});
+
+stop.addEventListener('click', () => {
+    cancelAnimationFrame(flyInterval);
+    stop.style.display = 'none';
+    start.style.display = 'block';
+});
+
+reset.addEventListener('click', () => {
+    cancelAnimationFrame(flyInterval);
+    stop.style.display = 'none';
+    start.style.display = 'block';
+    fly.style.left = 0 + 'px';
+    fly.style.top = 0 + 'px';
+    count = 0;
+});
