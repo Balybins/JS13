97bfc66cc2df53d9cae629ffa17200dee6fb1dc3
Stanislav Balybin <balybins@mail.ru>
2020-07-04 01:24:05 +0600

    Начало выполнения 22го урока

---
+++ js/script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+class Todo {
+    constructor(form, input, todoList, todoCompleted) {
+        this.form = document.querySelector(form);
+        this.input = document.querySelector(input);
+        this.todoList = document.querySelector(todoList);
+        this.todoCompleted = document.querySelector(todoCompleted);
+        this.todoData = new Map(JSON.parse(localStorage.getItem('todoList')));
+    }
+
+    addToStorage() {
+        localStorage.setItem('todoList', JSON.stringify([...this.todoData]));
+    }
+
+    render() {
+        this.todoList.textContent = '';
+        this.todoCompleted.textContent = '';
+        this.todoData.forEach(this.createItem);
+        this.addToStorage();
+    }
+
+    createItem = (todo) => {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.key = todo.key;
+        li.insertAdjacentHTML('beforeend', `
+            <span class="text-todo">${todo.value}</span>
+            <div class="todo-buttons">
+                <button class="todo-remove"></button>
+                <button class="todo-complete"></button>
+            </div>
+        `);
+        
+        if (todo.completed) {
+            this.todoCompleted.append(li);
+        } else {
+            this.todoList.append(li);
+        }
+    }
+
+    addTodo(e) {
+        e.preventDefault();
+
+        if (this.input.value.trim()) {
+            const newTodo = {
+                value: this.input.value,
+                completed: false,
+                key: this.generateKey(),
+            }
+            this.todoData.set(newTodo.key, newTodo);
+            this.render();
+        }
+    }
+
+    generateKey() {
+        return Math.random().toString(32).substring(2, 15) + Math.random().toString(32).substring(2, 15);
+    }
+
+    completedItem() {
+
+    }
+
+    deleteItem() {
+
+    }
+
+    handler() {
+
+    }
+
+    init() {
+        this.form.addEventListener('submit', this.addTodo.bind(this));
+        this.render();
+    }
+}
+
+const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
+
+todo.init();
