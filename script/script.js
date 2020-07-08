7b7988394013c407bbef62cce2812a97d628e6f7
Balybin Stanislav <balybins@mail.ru>
2020-07-09 01:12:35 +0600

    Выполнил обязательное задание 24го урока

--- script/script.js
+++ script/script.js
@@ -335,9 +335,9 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 	// Only numbers
-	const calc = document.getElementById('calc');
+	const calcInputs = document.getElementById('calc');
 
-	calc.addEventListener('click', event => {
+	calcInputs.addEventListener('click', event => {
 		if (event.target.tagName === 'INPUT') {
 			event.target.addEventListener('input', () => {
 				const notNumbers = /\D/;
@@ -345,4 +345,45 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+
+	// Калькулятор
+	const calc = (price = 100) => {
+		const calcBlock = document.querySelector('.calc-block'),
+			calcType = document.querySelector('.calc-type'),
+			calcSquare = document.querySelector('.calc-square'),
+			calcDay = document.querySelector('.calc-day'),
+			calcCount = document.querySelector('.calc-count'),
+			totalValue = document.getElementById('total');
+
+		const countSum = () => {
+			let total = 0,
+				countValue = 1,
+				dayValue = 1;
+			const typeValue = calcType.options[calcType.selectedIndex].value,
+				squareValue = +calcSquare.value;
+
+			if (calcCount.value > 1) {
+				countValue += (calcCount.value - 1) / 10;
+			}
+
+			if (calcDay.value && calcDay.value < 5) {
+				dayValue *= 2;
+			} else if (calcDay.value && calcDay.value < 10) {
+				dayValue *= 1.5;
+			}
+
+			if (typeValue && squareValue) {
+				total = price * typeValue * squareValue * countValue * dayValue;
+			}
+			totalValue.textContent = total;
+		};
+
+		calcBlock.addEventListener('change', (event) => {
+			const target = event.target;
+			if (target.matches('select') || target.matches('input')) {
+				countSum();
+			}
+		});
+	};
+	calc(100);
 });
