4a98b1055736e2426838299b22575bd3ed27331d
Balybin Stanislav <balybins@mail.ru>
2020-07-14 19:40:26 +0600

    В процессе выполнения задания

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
 
     const select = document.getElementById('cars'),
         output = document.getElementById('output');
 
-    select.addEventListener('change', () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {brand, model, price} = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
+    const chooseCar = () => {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', './cars.json');
+            request.setRequestHeader('Content-type', 'application/json');
+            request.send();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState === 4 && request.status === 200 && select.value !== 'no') {
+                    const data = JSON.parse(request.responseText);
+                    console.log(request.readyState);
+                    console.log(request.status);
+                    console.log(data);
+                    resolve(data);
+                } else {
+                    console.log('Произошла ошибка');
+                    reject();
+                }
+            });
+        });
+    };
+
+    const showCar = data => {
+        console.warn(data);
+        console.warn(select.value);
+        data.cars.forEach(item => {
+            if (item.brand === select.value) {
+                const { brand, model, price } = item;
+                output.innerHTML = `Тачка ${brand} ${model} <br>
                         Цена: ${price}$`;
-                    }
-                });
-            } else {
-                output.innerHTML = 'Произошла ошибка';
             }
         });
-    });
+    };
 
+    const error = () => {
+        output.innerHTML = 'Произошла ошибка';
+    };
+
+    select.addEventListener('change', () => {
+        chooseCar()
+            .then(showCar)
+            .catch(error);
+    });
 });
