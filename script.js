1b2008279d645ae09b45e196764701d2378e5581
Balybin Stanislav <balybins@mail.ru>
2020-07-14 20:30:53 +0600

    Выполнил дополнительно задание 28го урока

--- script.js
+++ script.js
@@ -5,28 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const chooseCar = () => {
         return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.open('GET', './cars.json');
-            request.setRequestHeader('Content-type', 'application/json');
-            request.send();
-            request.addEventListener('readystatechange', () => {
-                if (request.readyState === 4 && request.status === 200 && select.value !== 'no') {
-                    const data = JSON.parse(request.responseText);
-                    console.log(request.readyState);
-                    console.log(request.status);
-                    console.log(data);
-                    resolve(data);
-                } else {
-                    console.log('Произошла ошибка');
-                    reject();
-                }
-            });
+            if (select.value !== 'no') {
+                const request = new XMLHttpRequest();
+                request.open('GET', './cars.json');
+                request.setRequestHeader('Content-type', 'application/json');
+                request.send();
+                request.addEventListener('readystatechange', () => {
+                    if (request.readyState !== 4) {
+                        return;
+                    }
+                    if (request.status === 200) {
+                        const data = JSON.parse(request.responseText);
+                        resolve(data);
+                    } else {
+                        const error = 'Призошла ошибка!';
+                        reject(error);
+                    }
+                });
+            } else {
+                const error = 'Выберите авто!';
+                reject(error);
+            }
         });
     };
 
     const showCar = data => {
-        console.warn(data);
-        console.warn(select.value);
         data.cars.forEach(item => {
             if (item.brand === select.value) {
                 const { brand, model, price } = item;
@@ -36,9 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    const error = () => {
-        output.innerHTML = 'Произошла ошибка';
-    };
+    const error = error => output.innerHTML = error;
 
     select.addEventListener('change', () => {
         chooseCar()
